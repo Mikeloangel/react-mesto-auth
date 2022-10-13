629816c8b102188164398ac2ae049c5e83209564
Mikhail Varushichev <Mikeloangel@gmail.com>
2022-10-13 11:42:46 +0300

    refactor: apiAuth uses one function to fetch

--- src/utils/ApiAuth.js
+++ src/utils/ApiAuth.js
@@ -2,14 +2,21 @@ import parseErrorMessage from "./parseErrorMessage";
 
 export const BASE_URL = 'https://auth.nomoreparties.co';
 
+const fetchAuthApi = async (endpoint, method, body = null, headers = { 'Content-Type': 'application/json' }) => {
+  const config = {
+    method,
+    headers
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  return await fetch(`${BASE_URL}/${endpoint}`, config);
+}
+
 export const register = async (email, password) => {
-  const res = await fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ password, email })
-  });
+  const res = await fetchAuthApi('signup', 'post', { password, email });
 
   if (res.status === 201) {
     return res.json();
@@ -20,14 +27,7 @@ export const register = async (email, password) => {
 }
 
 export const authorization = async (email, password) => {
-  const res = await fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ password, email })
-  });
+  const res = await fetchAuthApi('signin', 'post', { password, email });
 
   if (res.status === 200) {
     const data = await res.json();
@@ -41,16 +41,11 @@ export const authorization = async (email, password) => {
 }
 
 export const checkToken = async (token) => {
-  const res = await fetch(
-    `${BASE_URL}/users/me`,
+  const res = await fetchAuthApi('users/me', 'get', null,
     {
-      method: 'get',
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
-    }
-  );
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    });
 
   if (res.status === 200) {
     const userData = await res.json();
@@ -61,6 +56,6 @@ export const checkToken = async (token) => {
   return Promise.reject(errorMessage);
 }
 
-export const forgetToken = () => {
-  localStorage.removeItem('token');
+export const forgetToken = (tokenFieldName = 'token') => {
+  localStorage.removeItem(tokenFieldName);
 }
